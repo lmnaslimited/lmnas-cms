d79c7ff2e23fad578e85fe4bf9cc8651850a2690
meharaj-thoufik <meharaj.thoufik.lmnas.com>
2024-09-04 14:54:34 +0530

    after codemods

--- v3/config/server.js
+++ v3/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a22b99afc445647e44aaa172f1996e7f'),
+    },
   },
 });
