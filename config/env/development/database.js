34514581e904807ab3d2081a96e0b8f93ba17e6f
krishnagovarthini-lmnas <krishnagovarthini.thirumurugan@lmnas.com>
2024-10-04 17:38:23 +0530

    fix : setup develop option

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,13 +1,11 @@
-module.exports =  ({ env }) => ({
+const path = require('path');
+
+module.exports = ({ env }) => ({
 	connection: {
-		client: 'postgres',
+		client: 'sqlite',
 		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
-			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME'),
-			user: env('DATABASE_USERNAME'),
-			password: env('DATABASE_PASSWORD'),
-			ssl: env.bool('DATABASE_SSL', false)
-		}
-	}
+			filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+		},
+		useNullAsDefault: true,
+	},
 });
